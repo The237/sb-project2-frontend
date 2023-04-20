bbd1deb0b73066b18b5290a903646919bf1bdc91
The237 <69681767+The237@users.noreply.github.com>
2023-04-20 13:37:31 +0100

    Add employee details visualisation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,9 @@ const routes: Routes = [
   },
   {
     path:"update-employee/:id", component: UpdateEmployeeComponent
+  },
+  {
+    path:"employee-details/:id", component: EmployeeDetailsComponent
   }
 ];
 
